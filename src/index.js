e2dcf75349f948d6acd66de910565c281800d0c0
SimoBekacem <SimoBekacem>
2024-05-11 21:53:06 +0100

    creating the route

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import First from './pages/First';
+import Home from './pages/Home.page';
 
 const router = createBrowserRouter([
 	{
 		path: '/first',
 		element: <First />,
+	},
+	{
+		path: '/home',
+		element: <Home />,
 	},
 ]);
 
@@ -16,8 +22,3 @@ root.render(
 		<RouterProvider router={router} />
 	</React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
