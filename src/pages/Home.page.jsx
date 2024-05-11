50899f479857f6d6f85e5499c061f1c8cb194b65
SimoBekacem <simohassane2002@gmail.com>
2024-05-11 21:26:36 +0100

    creating the home component files

---
+++ src/pages/Home.page.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div>
+            <div>
+                header
+            </div>
+            <div>
+                navigation bar
+            </div>
+            <div>
+                body
+            </div>
+        </div>
+    )
+
+}
