d091530ff16e8fb3f35be9e78d40d5a43b75a36d
SimoBekacem <SimoBekacem>
2024-05-12 00:32:30 +0100

    fixing css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import First from './pages/First';
+import First from './pages/First/First';
 import Home from './pages/Home/Home.page';
 
 const router = createBrowserRouter([
--- src/pages/First/First.jsx
+++ src/pages/First/First.jsx
@@ -1,46 +1,46 @@
-import React from "react";
-import "./First.css";
-import logo from "../assets/logo.png"
-import vector from "../assets/Vector.svg"
-import logo2 from "../assets/CardImage1.png"
-import electrician from "../assets/electrician.png"
+import React from 'react';
+import './First.css';
+import logo from '../../assets/logo.png';
+import vector from '../../assets/Vector.svg';
+import logo2 from '../../assets/CardImage1.png';
+import electrician from '../../assets/electrician.png';
 
-const First =() =>{
-return(
-    <div className="body">
-        <div class="overlay"></div>
-        <div className="menu">
-            <img src={logo} className="logo"></img>
-            <button >Why Us ?</button>
-            <button >Sign Up</button>
-            <button >Log In</button>
-        </div>
-        <div className="paragraph">
-            <p>Make the
-                difference 
-                and live the challenge</p>
-            <img src={vector}></img>
-        </div>
-        <div className="card">
-            <div className="part1">
-                <img src={logo2}></img>
-                <p>Share your<br/> <b>Energy</b> and<br/> gain <b>money</b></p>
-                <button>Start</button>
-            </div>
-            <div className="part2">
-                <img src={electrician}></img>
-            </div>
-        </div>
-        <div className="menu2">
-            <img src={logo} className="logo"></img>
-            <button >Why Us ?</button>
-            <button >Sign Up</button>
-            <button >Log In</button>
-        </div>
-    </div>
-
-)
-
-}
+const First = () => {
+	return (
+		<div className='body2'>
+			<div class='overlay'></div>
+			<div className='menu'>
+				<img src={logo} className='logo' alt='logo' />
+				<button>Why Us ?</button>
+				<button>Sign Up</button>
+				<button>Log In</button>
+			</div>
+			<div className='paragraph'>
+				<p>Make the difference and live the challenge</p>
+				<img src={vector} alt='logo' />
+			</div>
+			<div className='card'>
+				<div className='part1'>
+					<img src={logo2} alt='logo' />
+					<p>
+						Share your
+						<br /> <b>Energy</b> and
+						<br /> gain <b>money</b>
+					</p>
+					<button>Start</button>
+				</div>
+				<div className='part2'>
+					<img src={electrician} alt='logo' />
+				</div>
+			</div>
+			<div className='menu2'>
+				<img src={logo} className='logo' alt='logo' />
+				<button>Why Us ?</button>
+				<button>Sign Up</button>
+				<button>Log In</button>
+			</div>
+		</div>
+	);
+};
 
 export default First;
